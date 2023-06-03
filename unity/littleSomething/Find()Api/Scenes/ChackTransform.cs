8d0c711e90cb9580b74cbad21ed5b4564cfa5d07
Yiyi77644 <129649535+Yiyi77644@users.noreply.github.com>
2023-06-03 13:30:28 +0800

    Add files via upload

---
+++ unity/littleSomething/Find()Api/Scenes/ChackTransform.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChackTransform : MonoBehaviour
+{
+    void Start()
+    {
+        //Transform go_1 = transform.root;
+
+        //Transform go1_1 = transform.root.Find("littlegrandpa");
+        //Transform go1_2 = transform.Find("littlegrandpa");
+        //GameObject go1_3 = GameObject.Find("littlegrandpa");
+
+        //bool igo1_1 = go1_1.gameObject.activeSelf;
+        //bool igo1_2 = go1_1.gameObject.activeInHierarchy;
+        //Debug.Log(go1_1 + "：" + igo1_1);
+        //Debug.Log(go1_1 + "：" + igo1_2);
+
+        //Transform go2_1 = transform.root.Find("grandpa");
+        //Transform go2_2 = transform.Find("grandpa");
+        //GameObject go2_3 = GameObject.Find("grandpa");
+
+        //Transform go3_1 = transform.root.Find("uncle");
+        //Transform go3_2 = transform.Find("uncle");
+        //GameObject go3_3 = GameObject.Find("uncle");
+
+        //Transform go4_1 = transform.root.Find("papa");
+        //Transform go4_2 = transform.Find("papa");
+        //GameObject go4_3 = GameObject.Find("papa");
+
+        //Transform go5_1 = transform.root.Find("brother");
+        //Transform go5_2 = transform.Find("brother");
+        //GameObject go5_3 = GameObject.Find("brother");
+
+        //Transform go6_1 = transform.root.Find("me");
+        //Transform go6_2 = transform.Find("me");
+        //GameObject go6_3 = GameObject.Find("me");
+
+        //Transform go7_1 = transform.root.Find("son");
+        //Transform go7_2 = transform.Find("son");
+        //GameObject go7_3 = GameObject.Find("son");
+
+        Transform go8_1 = transform.root.Find("grandson");
+        Transform go8_2 = transform.Find("grandson");
+        GameObject go8_3 = GameObject.Find("grandson");
+
+
+    }
+
+}
